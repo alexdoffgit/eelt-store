97d4df4be4583f94e2de087076647e7d70e50845
alexdoffgit <noreply@plea.se>
2022-05-24 15:41:04 +0700

    add product interface as dependency

--- backend/port/http/product/list.ts
+++ backend/port/http/product/list.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import { IProduct } from "../../../domain/model/product";
 
-export function productList() {
+export function productList(store: IProduct) {
     return async function(req: Request, res: Response) {
         
     } 
