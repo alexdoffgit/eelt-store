a148b821f0894f0bace217b67f610fcfab034b59
alexdoffgit <noreply@plea.se>
2022-05-25 01:20:22 +0700

    add product page minimal

---
+++ frontend/src/Product.svelte
@@ -0,0 +1,20 @@
+<script>
+import axios from "axios";
+import { SERVER_URL } from "../env";
+export let params
+let product
+
+async function fetchFilled() {
+    let res = axios.get(`${SERVER_URL}/product/${params.id}`)
+}
+
+async function submitStuff() {
+
+}
+</script>
+
+{#if params}
+    <form on:submit|preventDefault={submitStuff}>
+        
+    </form>
+{/if}
