aa942717af75406c998cbc2826a6f1e80922ff4d
alexdoffgit <noreply@plea.se>
2022-05-24 15:33:04 +0700

    implements getAll

--- backend/adapter/prisma-mysql/product.ts
+++ backend/adapter/prisma-mysql/product.ts
@@ -1,3 +1,24 @@
-export class ProductImplemented {
-    
+import { IProduct, Product } from "../../domain/model/product";
+import { prisma } from "./prisma";
+
+export class ProductImplemented implements IProduct {
+    async getAll() {
+        let productsPrisma = await prisma.product.findMany()
+
+        return productsPrisma.map(p => {
+            let products: Product = {
+                id: p.id,
+                cartDesc: p.cartDesc,
+                imagePath: p.imagePath,
+                longDesc: p.longDesc,
+                name: p.name,
+                price: p.price,
+                published: p.published,
+                sku: p.sku,
+                stock: p.stock
+            }
+
+            return products
+        })
+    }
 }
