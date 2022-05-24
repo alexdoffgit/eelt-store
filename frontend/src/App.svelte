ec3cd57bdd5837360c3cf35dc9c8f46fc36602c1
alexdoffgit <noreply@plea.se>
2022-05-25 01:00:56 +0700

    create route in main

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,16 @@
 <script>
 import ProductList from "./ProductList.svelte";
+import page from "page";
+
+let current
+let params
+
+page("/products", () => current = ProductList)
+page("/product/:id", (ctx, next) => {
+    params = ctx.params
+    next()
+}, () => current = Product)
+
 </script>
-	
-<ProductList />
+
+<svelte:component this={current} {params} />
