9a297ffdf3a0fa69cc31435407c688957e153560
alexdoffgit <noreply@plea.se>
2022-05-24 18:52:36 +0700

    implement product list handler

--- backend/port/http/product/list.ts
+++ backend/port/http/product/list.ts
@@ -23,6 +23,38 @@ function productListAdapter(
 
 export function productList(store: IProduct) {
     return async function(req: Request, res: Response) {
-        
+        try {
+            const productsStore = await store.getAll()
+            
+            if(productsStore.length === 0) {
+                res.json({
+                    products: []
+                })
+            } else {
+                const addr = process.env.SERVER_ADDR
+                const port = process.env.SERVER_PORT
+                
+                if(addr && port) {
+                    res.json({
+                        products: productsStore.map(
+                            p => productListAdapter(p, addr, port)
+                        ) 
+                    })
+                } else {
+                    console.error("SERVER_ADDR and SERVER_PORT env is empty")
+                    res.send("internal server error")
+                }
+            }
+        } catch(e) {
+            if(e instanceof PrismaClientKnownRequestError) {
+                res.status(500)
+                console.error(`${e.code}: ${e.message}`)
+                res.send("internal server error")
+            } else {
+                res.status(500)
+                console.error((e as any).message)
+                res.send("internal server error")
+            }
+        }
     } 
 }
