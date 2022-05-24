997dd7bb75ec86fb5d8af5d8bab98c5b6dd006e4
alexdoffgit <noreply@plea.se>
2022-05-24 18:37:53 +0700

    add image endpoint

---
+++ backend/port/http/product/image.ts
@@ -0,0 +1,22 @@
+import { IProduct } from "../../../domain/model/product";
+import { Request, Response } from "express";
+import { ImageFileNotFound } from "../../../domain/error";
+
+export function productImage(store: IProduct) {
+    return async function(req: Request, res: Response) {
+        try {
+            const imagePath = await store.getFilePathById(Number(req.params.id))
+            res.sendFile(imagePath)
+        } catch(e) {
+            if(e instanceof ImageFileNotFound) {
+                res.status(e.statusCode)
+                console.error(e.message)
+                res.send(e.message)
+            } else {
+                res.status(500)
+                console.error((e as any).message)
+                res.send("internal server error")
+            }
+        }
+    }
+}
