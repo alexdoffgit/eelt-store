00fda84a230695ee2996f9d1296014fc64381a9d
alexdoffgit <noreply@plea.se>
2022-05-24 16:32:30 +0700

    create an adapter function

--- backend/port/http/product/list.ts
+++ backend/port/http/product/list.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from "express";
-import { IProduct } from "../../../domain/model/product";
+import { ProductListResponse } from "../../../domain/dto/product";
+import { IProduct, Product } from "../../../domain/model/product";
+
+function productListAdapter(p: Product): ProductListResponse | null {
+    const address = process.env.SERVER_ADDR
+    const port = process.env.SERVER_PORT
+    if(address && port) {
+        return {
+            id: p.id,
+            cartDesc: p.cartDesc,
+            desc: p.longDesc,
+            imageUrl: `${address}:${port}/product/image/${p.id}`,
+            price: p.price,
+            published: p.published,
+            sku: p.sku,
+            stock: p.sku
+        }
+    }
+
+    return null
+}
 
 export function productList(store: IProduct) {
     return async function(req: Request, res: Response) {
