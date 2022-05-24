24b273fbfc994933527e4d4795563eda0a19d980
alexdoffgit <noreply@plea.se>
2022-05-25 01:20:10 +0700

    add fallback page as home

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
+import Fallback from "./Fallback.svelte";
 import ProductList from "./ProductList.svelte";
 import page from "page";
 
 let current
 let params
 
+page("/", () => current = Fallback)
 page("/products", () => current = ProductList)
 page("/product/:id", (ctx, next) => {
     params = ctx.params
